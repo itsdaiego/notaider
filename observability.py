354b1cead03c12c6449de3e4c572d83237a67ae0
Dyego Rodrigo <dyego9444@gmail.com>
2026-07-08 01:12:03 -0300

    remove unnecessary global

--- observability.py
+++ observability.py
@@ -2,12 +2,8 @@ import os
 
 from langfuse import get_client
 
-_initialized = False
-
 
 def init_observability() -> None:
-    global _initialized
-    if _initialized or not os.getenv("LANGFUSE_PUBLIC_KEY"):
+    if not os.getenv("LANGFUSE_PUBLIC_KEY"):
         return
     get_client()  # reads LANGFUSE_* env vars, registers global OTel tracer provider
-    _initialized = True
